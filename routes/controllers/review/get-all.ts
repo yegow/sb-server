f8ed73d85f119331f3e2c7b9159ff0904785b9ae
yegow <33445976+yegow@users.noreply.github.com>
2019-10-28 11:14:41 +0300

    Implemented Sequelize join on reviews (#1)

--- routes/controllers/review/get-all.ts
+++ routes/controllers/review/get-all.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
+
 import { createResponse } from "../create-response";
 import { Review } from "../../../models/review";
+import { User } from "../../../models/user";
+import { Property } from "../../../models/property";
 
 const getAll = async (req: Request, res: Response) => {
     const {user, property, limit, sort} = req.query,
@@ -18,6 +21,19 @@ const getAll = async (req: Request, res: Response) => {
     if (sort) {
         opts.order = sort;
     }
+
+    opts.include = [
+        {
+            model: User,
+            where: { id: Sequelize.col('review.userId')},
+            include: ['username', 'id']
+        },
+        {
+            model: Property,
+            where: { id: Sequelize.col('review.propertyId')},
+            include: ['title', 'type', 'category', 'location', 'id']
+        }
+    ]
 
     try {
         const reviews = await Review.findAll(opts);
--- routes/controllers/review/get-one.ts
+++ routes/controllers/review/get-one.ts
@@ -1,10 +1,29 @@
 import { Request, Response } from "express";
 import { createResponse } from "../create-response";
 import { Review } from "../../../models/review";
+import { User } from "../../../models/user";
+import { Property } from "../../../models/property";
 
 const getOne = async (req: Request, res: Response) => {
     try {
-        const review = await Review.findByPk(req.params.id)
+        const review = await Review.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [
+                {
+                    model: User,
+                    where: { id: Sequelize.col('review.userId')},
+                    include: ['username', 'id']
+                },
+                {
+                    model: Property,
+                    where: { id: Sequelize.col('review.propertyId')},
+                    include: ['title', 'type', 'category', 'location', 'id']
+                }
+            ]
+        });
+
         if (!review) {
             return res.status(204).json(createResponse(
                 'fail',
