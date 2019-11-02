d0a7e7940969d08140edcc6bc09f93706a17733a
yegow <allanyego05@gmail.com>
2019-11-02 17:29:09 +0300

    Fixed coverage file in config.yml

---
+++ spec/propertySpec.js
@@ -0,0 +1,8 @@
+describe('/properties', () => {
+  describe('GET /', () => {
+    it('should respond with an array of properties', (done) => {
+      expect(true).toBe(true);
+      done();
+    });
+  })
+});
