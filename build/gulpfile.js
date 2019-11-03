5d39f1b88cebdd1bb387e0c097338382eb7a5108
yegow <allanyego05@gmail.com>
2019-11-03 10:11:58 +0300

    Deploy fix

--- build/gulpfile.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var gulp_1 = require("gulp");
-gulp_1.task('build.copy.assets', function () {
-    return gulp_1.src(['./**/*', '!./**/*.ts', '!node_modules', '!coverage'])
-        .pipe(gulp_1.dest('build'));
-});
-gulp_1.task('default', gulp_1.series('build.copy.assets'));
-//# sourceMappingURL=gulpfile.js.map
